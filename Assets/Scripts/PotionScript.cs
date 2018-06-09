7625c17b1c2dbd6a33488da64af1b630f1279d76
EmilyYVChan <emily97m2@gmail.com>
2018-06-09 19:39:53 +1200

    Add logic for mouse hover over a potion

---
+++ Assets/Scripts/PotionScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionScript : MonoBehaviour {
+
+    public Texture2D cursorClickableTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+    // Use this for initialization
+    void Start () {
+		Debug.Log ("PotionScript.start()");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseEnter()
+	{
+        Cursor.SetCursor(cursorClickableTexture, hotSpot, cursorMode);
+        Debug.Log ("PotionScript.OnMouseEnter()");
+	}
+
+	void OnMouseOver()
+	{
+		Debug.Log ("PotionScript.OnMouseOver()");
+	}
+
+	void OnMouseExit()
+	{
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
+		Debug.Log ("PotionScript.OnMouseExit()");
+	}
+}
