2a626d237491bbeb6ddac020efeb9eb857de5bd8
EmilyYVChan <emily97m2@gmail.com>
2018-06-10 18:12:21 +1200

    update tutorial graph to show expected inputs and outputs more comprehensively

---
+++ Assets/Scripts/InspectionScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InspectionScript : MonoBehaviour {
+
+    //Cost UI text
+
+    //
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+
+        //increase cost if not yet inspected
+        //show component description
+        Debug.Log("PotionScript.OnMouseDown()");
+    }
+}
