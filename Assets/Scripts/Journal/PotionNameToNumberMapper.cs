08da4869e393afd860e6381af218b76670b0e32c
EmilyYVChan <echa232@aucklanduni.ac.nz>
2018-07-10 11:25:55 +1200

    Add all potionNameToNumber mappings

--- Assets/Scripts/Journal/PotionNameToNumberMapper.cs
+++ Assets/Scripts/Journal/PotionNameToNumberMapper.cs
@@ -9,7 +9,24 @@ public static class PotionNameToNumberMapper {
             { "PinkPotion", 1 },
             { "RedPotion", 2 },
             { "YellowPotion", 3 },
-            { "BluePotion", 4 }
+            { "BluePotion", 4 },
+            { "LightPinkPotion", 5 },
+            { "PurplePotion", 6 },
+            { "PurplePotion1", 7 },
+            { "PurplePotion2", 8 },
+            { "GreenPotion", 9 },
+            { "GreenPotion1", 10 },
+            { "GreenPotion2", 11 },
+            { "RedPotion1", 12 },
+            { "RedPotion2", 13 },
+            { "NavyBluePotion", 14 },
+            { "DarkBluePotion", 15 },
+            { "TurquoisePotion", 16 },
+            { "DarkBluePotion", 15 },
+            { "OrangePotion1", 17 },
+            { "OrangePotion2", 18 },
+            { "OrangePotion3", 19 },
+            { "LightBrownPotion", 20 }
         };
 
     public static int getPotionNumber(string potionName)
