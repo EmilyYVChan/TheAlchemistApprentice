2e326e7717185b6a69e40be6a047da9682c4287d
Unknown <cli727@aucklanduni.ac.nz>
2018-07-13 23:19:24 +1200

    Update CanvasResult to contain game stats

--- Assets/Scripts/DiagnosePotionScript.cs
+++ Assets/Scripts/DiagnosePotionScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DiagnosePotionScript : ExecutePotionScript {
 	public bool isFault;
@@ -15,8 +16,34 @@ public class DiagnosePotionScript : ExecutePotionScript {
 	{
 		if (isFault) {
 			canvasResult.SetActive(true);
+	
+			int totalCost = LevelData.GetTotalCost ();
+
+			Text mana = GameObject.Find ("Mana").GetComponent<Text>();
+			mana.text = totalCost.ToString();
+
+			Text iteration = GameObject.Find ("Iteration").GetComponent<Text>();
+			iteration.text = LevelData.getCurrentIteration ().ToString();
+
+			ResultBoundary rb = canvasResult.GetComponent<ResultBoundary> ();
+			int lowerBound = rb.lowerBoundary;
+			int upperBound = rb.upperBoundary;
+
+			if (totalCost <= lowerBound) {
+				GameObject.Find ("OneStar").SetActive (false);
+				GameObject.Find ("TwoStars").SetActive (false);
+			} else if ((lowerBound < totalCost) && (totalCost <= upperBound)) {
+				GameObject.Find ("OneStar").SetActive (false);
+				GameObject.Find ("ThreeStars").SetActive (false);
+			} else {
+				// totalCost > upperBoudn
+				GameObject.Find ("ThreeStars").SetActive (false);
+				GameObject.Find ("TwoStars").SetActive (false);
+			}
+
 		} else {
-			Debug.Log ("not fault");
+			Text costTextUI = GameObject.Find("Cost").GetComponent<Text>();
+			LevelData.addCost(int.Parse(costTextUI.text) * -1);
 		}
 	}
 }
