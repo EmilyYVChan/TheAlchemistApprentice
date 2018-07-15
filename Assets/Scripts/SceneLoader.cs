16d38276398a4c3d8b8ef30b1612ef420255c834
EmilyYVChan <echa232@aucklanduni.ac.nz>
2018-07-16 07:31:46 +1200

    'Revert' my changes to SceneLoader.cs

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,74 +5,29 @@ using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
-    private string executeSceneName = "TutorialExecuteScene";
-    private string inspectSceneName = "TutorialScene";
+	private string executeSceneName = "TutorialExecuteScene";
+	private string inspectSceneName = "TutorialScene";
 
-    public void loadScene(int sceneNumber)
-    {
-		if (shouldIncrementIterationCount (sceneNumber)) {
-			LevelData.incrementIteration ();
+	public void loadScene(int sceneNumber)
+	{
+		SceneManager.LoadScene(sceneNumber);
+	}
 
-			if (LevelData.getCurrentIteration() == 2)
-			{
-				TutorialManagerInspect.setIsSecondIteration();
-			}
-		} else if (shouldResetIterationCount (sceneNumber)) {
-			LevelData.resetIterationCount ();
-		}
-        SceneManager.LoadScene(sceneNumber);
-    }
+	public void loadTutorialInspectScene()
+	{
+		Debug.Log ("load tutorial inspect?");
+		SceneManager.LoadScene(inspectSceneName);
+		LevelData.incrementIteration();
 
-    public void loadTutorialInspectScene()
-    {
-        SceneManager.LoadScene(inspectSceneName);
-        LevelData.incrementIteration();
-
-        if (LevelData.getCurrentIteration() == 2)
-        {
-            TutorialManagerInspect.setIsSecondIteration();
-        }
-    }
-
-    public void loadTutorialExecuteScene()
-    {
-        SceneManager.LoadScene(executeSceneName);
-    }
-
-	private bool shouldIncrementIterationCount(int nextSceneNumber) {
-		int currentSceneNumber = SceneManager.GetActiveScene ().buildIndex;
-
-		if (isAnExecuteScene(currentSceneNumber) && isSameLevel(nextSceneNumber, currentSceneNumber)) {
-			return true;
-		} else {
-			return false;
+		if (LevelData.getCurrentIteration() == 2)
+		{
+			TutorialManagerInspect.setIsSecondIteration();
 		}
 	}
 
-	private bool isAnExecuteScene(int sceneNumber) {
-		if (sceneNumber == 2 || sceneNumber == 4 || sceneNumber == 6) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	private bool isSameLevel(int inspectSceneNumber, int executeSceneNumber) {
-		if (inspectSceneNumber == (executeSceneNumber - 1)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	private bool shouldResetIterationCount(int nextSceneNumber) {
-		int currentSceneNumber = SceneManager.GetActiveScene ().buildIndex;
-
-		if (isAnExecuteScene (currentSceneNumber) && !isSameLevel (nextSceneNumber, currentSceneNumber)) {
-			return true;
-		} else {
-			return false;
-		}
+	public void loadTutorialExecuteScene()
+	{
+		SceneManager.LoadScene(executeSceneName);
 	}
 
 	public void clearLevelDataUponLoadingNextLevel(){
